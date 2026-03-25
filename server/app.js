32c7f536c54b5045ba4ab9dd0f3f1ad3c41d11da
Kris Sherbondy <k.sherbondy86@gmail.com>
2026-03-25 02:28:51 -0500

    Completed all logic for routes. All tests are passing.

--- server/app.js
+++ server/app.js
@@ -3,11 +3,14 @@ const morgan = require('morgan');
 const fs = require('fs').promises;
 const path = require('path');
 const app = express();
+const bodyparser = require('body-parser');
 
 
 
 // add your code here
 app.use(logRequests);
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
 
 /**
  * GET ROUTES
@@ -17,7 +20,7 @@ app.get('/', (req, res) => {
     res.status(200).json({status: 'ok'});
 });
 
-app.get('/api/TodoItems', async (req, res) => {
+app.get('/api/TodoItems', async (req, res, next) => {
     // Expects all TODO items from array & status 200
     try {
         // Read and parse in the contents of data.json
@@ -29,18 +32,18 @@ app.get('/api/TodoItems', async (req, res) => {
     }
 });
 
-app.get('/api/TodoItems/:number', async (req, res) => {
+app.get('/api/TodoItems/:number', async (req, res, next) => {
     // Expects all TODO items from array & status 200
     try {
         // Grab the id using params where :number from the route is the param identifier and type
-        const itemNum = req.params.number;
+        const itemNum = parseInt(req.params.number);
         // Read and parse data in the contents of data.json
         const todos = await getJSONData();
-        const item = todos[itemNum];
+        const item = todos.find(todo => todo.todoItemId === itemNum);
+        // Return what the client asked for
         if (!item) {
             return res.status(404).send('Item not found');
         }
-        // Return what the client asked for
         res.status(200).json(item);
     } catch (err) {
         next(err);
@@ -50,20 +53,29 @@ app.get('/api/TodoItems/:number', async (req, res) => {
 /**
  * POST ROUTES
  */
-app.post('/api/TodoItems', (req, res) => {
-    // Log the event or is this already logged in app.use()?
-    
+app.post('/api/TodoItems', async (req, res, next) => {
+    const postItem = req.body;
+    const todos = await getJSONData();
     // Push the new TODO item onto the array in the json file.
-   
+    await saveJSONData(postItem, todos);
+    res.status(201).json(postItem);
+    
     // Expects the item pushed and status of 201
-    res.status(201);
+    
 });
 
 
 /**
  * DELETE ROUTES
  */
-
+app.delete('/api/TodoItems/:number', async (req, res, next) => {
+    const deleteId = parseInt(req.params.number);
+    const todos = await getJSONData();
+    const deleteResp = todos[deleteId];
+    // Delete the TODO item from the array in the json file.
+    await deleteJSONData(deleteId, todos);
+    res.status(200).json(deleteResp);    
+});
 
 /**
  * FAIL ROUTE
@@ -96,6 +108,19 @@ async function getJSONData() {
     const fileData = await fs.readFile('./data.json', 'utf8');
     const todos = JSON.parse(fileData);
     return todos;
+}
+
+async function saveJSONData(obj, array) {
+    let newTodo = obj;
+    array.push(newTodo);
+    await fs.writeFile('./data.json', JSON.stringify(array, null, 2));
+}
+
+async function deleteJSONData(objNum, array) {
+    let deleteObj = objNum;
+    let newObjArray = array.filter(item => item.todoItemId !== deleteObj);
+    await fs.writeFile('./data.json', JSON.stringify(newObjArray, null, 2));
+    
 }
 
 module.exports = app;
